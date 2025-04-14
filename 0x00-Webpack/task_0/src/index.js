7be389998bbae7bf4eb04fc58864ca1941b7e3d9
= <=>
2025-04-14 05:01:41 -0700

    Basic setup

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,7 @@
+var jquery = require("jquery");
+
+$(function () {
+  $('body').append('<p>ALX Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<p>copyright - ALX</p>');
+});
